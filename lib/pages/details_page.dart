b07cea3f93cfe1ea34fcdc1420ea43c153e665de
hmartiins <hmartins224@gmail.com>
2023-12-31 22:50:53 -0300

    refactor: details page

--- lib/pages/details_page.dart
+++ lib/pages/details_page.dart
@@ -10,55 +10,94 @@ class DetailsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(movie.title),
+        title: Text(movie.title,
+            style: const TextStyle(fontWeight: FontWeight.bold)),
       ),
       body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(24.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              SizedBox(
-                height: MediaQuery.of(context).size.height * 0.55,
-                width: MediaQuery.of(context).size.width,
-                child: Hero(
-                  tag: movie.id,
-                  child: Image.network(
-                    API.REQUEST_IMG(movie.posterPath),
-                    loadingBuilder: (_, child, progress) {
-                      if (progress == null) return child;
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              width: MediaQuery.of(context).size.width,
+              child: Hero(
+                tag: movie.id,
+                child: Image.network(
+                  API.REQUEST_IMG(movie.backdropPath != null
+                      ? movie.backdropPath!
+                      : movie.posterPath),
+                  loadingBuilder: (_, child, progress) {
+                    if (progress == null) return child;
 
-                      return const CircularProgressIndicator.adaptive();
-                    },
-                  ),
+                    return const CircularProgressIndicator.adaptive();
+                  },
                 ),
               ),
-              const SizedBox(height: 20),
-              Text(
-                movie.overview,
-                style: Theme.of(context).textTheme.titleMedium,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                top: 24,
+                left: 24,
+                right: 24,
+                bottom: 8,
               ),
-              const SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              child: Column(
                 children: [
-                  const Icon(
-                    Icons.title,
+                  Row(
+                    children: [
+                      Text(
+                        movie.title,
+                        style: const TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.white,
+                        ),
+                      ),
+                      const SizedBox(width: 14),
+                      DecoratedBox(
+                        decoration: BoxDecoration(
+                          color: Colors.black87,
+                          borderRadius: BorderRadius.circular(4),
+                        ),
+                        child: const Padding(
+                          padding: EdgeInsets.symmetric(
+                            horizontal: 8,
+                            vertical: 4,
+                          ),
+                          child: Text("4K"),
+                        ),
+                      ),
+                    ],
                   ),
-                  Text(movie.originalTitle),
+                  const SizedBox(height: 10),
+                  Row(
+                    children: [
+                      const Icon(Icons.calendar_month, color: Colors.white70),
+                      const SizedBox(width: 4),
+                      Text(
+                        "${movie.releaseDate.day}/${movie.releaseDate.month}/${movie.releaseDate.year}",
+                        style: const TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.white60,
+                        ),
+                      ),
+                      const SizedBox(width: 30),
+                      const Icon(Icons.star, color: Colors.white60),
+                      const SizedBox(width: 4),
+                      Text(
+                        movie.voteAverage.toStringAsFixed(2),
+                        style: const TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.white60,
+                        ),
+                      ),
+                    ],
+                  )
                 ],
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Icon(
-                    Icons.date_range,
-                  ),
-                  Text(movie.releaseDate.toString()),
-                ],
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
